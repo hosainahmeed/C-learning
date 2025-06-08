1088cb6c9c4513b8e1157ca7358c66a4d761d5e2
hosain ali <hosainahmed534745@gmail.com>
2025-06-08 11:14:53 +0600

    mathematical convention

--- first.cpp
+++ first.cpp
@@ -15,9 +15,30 @@
 int main () {
     // const double pi = 3.14;
     // pi =3 ; //Cannot assign to readonly type const double
-    // naming convention type
+
+    //************* naming convention type *************
+
     // int file_size ; //snake case
     // int File_Size ; //pascal case
     // int fileSize ; //camel case
     // int iFileSize ; //hungarian notation case
+
+    //************* mathematical convention *************
+    double x = 10;
+    int y = 3 ;
+    // int z = x + y; // this is call addition
+    // int z = x - y; // this is call subtraction
+    // int z = x * y; // this is call multiplication
+    // double z = x / y; // this is cell division
+
+    // shortcut
+    // x = x + 5;
+    // and also we have increment and decrement
+    // x ++; // increment
+    // x --; // decreament
+
+    // std::cout << z ; // 3.33333
+
+
+    return 0 ;
 }
