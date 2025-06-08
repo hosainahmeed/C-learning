f5700fe6881080912c3e5bd6af4ed1af4d6d94cc
hosain ali <hosainahmed534745@gmail.com>
2025-06-08 11:03:21 +0600

    naming convention

--- first.cpp
+++ first.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 // swap the value a to b and b to a
-int main() {
-    int a = 2;
-    int b = 3 ;
-    int temp = a ;
-    a = b ;
-    b = temp ;
+// int main() {
+//     int a = 2;
+//     int b = 3 ;
+//     int temp = a ;
+//     a = b ;
+//     b = temp ;
+//
+//     std::cout << a ;
+//     return 0;
+// }
 
-    std::cout << a ;
-    return 0;
+
+int main () {
+    // const double pi = 3.14;
+    // pi =3 ; //Cannot assign to readonly type const double
+    // naming convention type
+    // int file_size ; //snake case
+    // int File_Size ; //pascal case
+    // int fileSize ; //camel case
+    // int iFileSize ; //hungarian notation case
 }
